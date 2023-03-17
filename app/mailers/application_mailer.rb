ca059edf1bd9197d7539df9047843f6cdd416b97
Ronat <ronat@techversantinfo.com>
2023-03-17 18:56:42 +0530

    Added pre commit hook and solved existing offences

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
+# It provides a simple and flexible way to create and send emails
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout "mailer"
+  default from: 'from@example.com'
+  layout 'mailer'
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# It provides a number of features that are common to all models
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end
