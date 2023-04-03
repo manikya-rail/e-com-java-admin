edaa2066fc9ad917989d523cc55b0d753e812825
BivinBv <bivin.b@techversantinfo.com>
2023-04-03 19:47:33 +0530

    login page validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, NgForm, Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  test : Date = new Date();
-  constructor() { }
+  test: Date = new Date();
+  form: any;
+  passwordRegex:string="/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/"
+  constructor() {
+
+    this.form = new FormGroup({
+      userName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        //Validators.pattern("/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/")
+      ])
+
+    });
+  }
+
+  get userName() {
+    return this.form.get('userName')
+  }
+
+  get password() {
+    return this.form.get('password')
+  }
+
+  onSubmit(f: NgForm) {
+    console.log(f);
+  }
+
+  getValue(f: FormControl) {
+    console.log(f);
+  }
 
   ngOnInit(): void {
   }
