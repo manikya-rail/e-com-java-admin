314ba64816455b5c4af94524b98f3dbcca669e9e
BivinBv <bivin.b@techversantinfo.com>
2023-07-28 14:28:39 +0530

    new layout

---
+++ src/layout/MainLayout/Drawer/DrawerHeader/index.js
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import { Stack } from '@mui/material';
+
+// project import
+import DrawerHeaderStyled from './DrawerHeaderStyled';
+import Logo from 'assets/images/logo/techlogo.png';
+
+import 'assets/css/navbar.css'
+
+// ==============================|| DRAWER HEADER ||============================== //
+
+const DrawerHeader = ({ open }) => {
+  const theme = useTheme();
+
+  return (
+    // only available in paid version
+    <DrawerHeaderStyled theme={theme} open={open}>
+      <Stack direction="row" spacing={1} alignItems="center">
+      <img src={Logo} alt='techversant' id='techversantLogo'
+      />
+        {/* <Chip
+          label={process.env.REACT_APP_VERSION}
+          size="small"
+          sx={{ height: 16, '& .MuiChip-label': { fontSize: '0.625rem', py: 0.25 } }}
+          component="a"
+          href="https://github.com/codedthemes/mantis-free-react-admin-template"
+          target="_blank"
+          clickable
+        /> */}
+      </Stack>
+    </DrawerHeaderStyled>
+  );
+};
+
+DrawerHeader.propTypes = {
+  open: PropTypes.bool
+};
+
+export default DrawerHeader;
