9cd3bb9d4384abaea2b7ed836603e5a41f1edb11
BivinBv <bivin.b@techversantinfo.com>
2023-08-01 20:07:45 +0530

    add client model

--- src/pages/components-overview/ClientList.js
+++ src/pages/components-overview/ClientList.js
@@ -1,13 +1,17 @@
 // material-ui
+import * as React from 'react';
 import {
   //  Breadcrumbs,
   //   Divider, 
 
   Grid,
   // Link,
-  Stack,Tooltip
+  Stack,Tooltip,DialogTitle,IconButton,Button,DialogContent,Typography,DialogActions,Dialog 
   // Typography 
 } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+
 
 
 
@@ -25,11 +29,51 @@ import { PlusSquare } from '../../../node_modules/react-bootstrap-icons/dist/ind
 
 // ==============================|| COMPONENTS - TYPOGRAPHY ||============================== //
 
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}));
 
+function BootstrapDialogTitle(props) {
+  const { children, onClose, ...other } = props;
+
+  return (
+    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+      {children}
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          {/* close */}
+        </IconButton>
+      ) : null}
+    </DialogTitle>
+  );
+}
 
 
 const ClientList = () => {
  
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return(
   <>
 
@@ -39,7 +83,7 @@ const ClientList = () => {
       </Grid> */}
       <Grid item xs={12}>
       <Tooltip title="Add Client">
-      <PlusSquare id="addClient"/>
+      <PlusSquare id="addClient"   onClick={handleClickOpen}/>
       </Tooltip>
       </Grid>
       <Grid item xs={12}>
@@ -48,6 +92,43 @@ const ClientList = () => {
       </MainCard>
       </Grid>
     </Grid>
+
+
+    <div>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Open dialog
+      </Button>
+      <BootstrapDialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <BootstrapDialogTitle id="customized-dialog-title" onClose={handleClose}>
+          Add client
+        </BootstrapDialogTitle>
+        <DialogContent dividers>
+          <Typography gutterBottom>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+            consectetur ac, vestibulum at eros.
+          </Typography>
+          <Typography gutterBottom>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          </Typography>
+          <Typography gutterBottom>
+            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
+            magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
+            ullamcorper nulla non metus auctor fringilla.
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose}>
+            Save changes
+          </Button>
+        </DialogActions>
+      </BootstrapDialog>
+    </div>
 
 
     <Grid container spacing={3}>
