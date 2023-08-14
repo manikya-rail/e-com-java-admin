6449388717ad76cefe097fb2913d69e3a43d98f4
Nayana Jayaraj <njayaraj50@gmail.com>
2023-08-14 18:46:11 +0530

    url changed

--- src/apiservices/config.js
+++ src/apiservices/config.js
@@ -3,12 +3,12 @@ const apiUrl = process.env.REACT_APP_API_URL;
 const config = {
   loginurl: `${apiUrl}api/auth/login`,
   addClientUrl: `${apiUrl}api/auth/client/register`,
-  getClientdetailsByIdUrl: `${apiUrl}api/auth`,
+  getClientdetailsByIdUrl: `${apiUrl}api/auth/client`,
   getClientListUrl: `${apiUrl}api/auth/client`,
   getClientImageUrl: `${apiUrl}api/auth/image`,
-  getSuperAdminProfile: `${apiUrl}api/auth`,
-  editProfileUrl: `${apiUrl}api/auth/client`,
-  deleteClientUrl: `${apiUrl}api/auth/client`
+  getSuperAdminProfile: `${apiUrl}api/auth/client`,
+  editProfileUrl: `${apiUrl}api/auth/client/edit`,
+  deleteClientUrl: `${apiUrl}api/auth/client/delete`
 };
 
 export default config;
